0e22ae7bb40a5f59e3fc65ee40da1edefc185fae
abhishekvarmagh <abhishektheverma@gmail.com>
2020-03-18 23:40:28 +0530

    ADDED : Perform Equality Test For Inch

--- src/main/java/quantitymeasurement/Inch.java
+++ src/main/java/quantitymeasurement/Inch.java
@@ -2,11 +2,10 @@ package quantitymeasurement;
 
 import java.util.Objects;
 
-public class Feet {
-
+public class Inch {
     private double value;
 
-    public Feet(double value) {
+    public Inch(double value) {
         this.value = value;
     }
 
@@ -14,8 +13,8 @@ public class Feet {
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        Feet feet = (Feet) o;
-        return Double.compare(feet.value, value) == 0;
+        Inch inch = (Inch) o;
+        return Double.compare(inch.value, value) == 0;
     }
 
 }
--- src/test/java/quantitymeasurement/QuantityMeasurementTest.java
+++ src/test/java/quantitymeasurement/QuantityMeasurementTest.java
@@ -32,4 +32,32 @@ public class QuantityMeasurementTest {
         Feet feet2 = null;
         Assert.assertNotEquals(feet1,feet2);
     }
+
+    @Test
+    public void given0InchAnd0Inch_ShouldReturnEqual() {
+        Inch inch1 = new Inch(0.0);
+        Inch inch2 = new Inch(0.0);
+        Assert.assertEquals(inch1,inch2);
+    }
+
+    @Test
+    public void given0InchAnd1Inch_ShouldReturnNotEqual() {
+        Inch inch1 = new Inch(0.0);
+        Inch inch2 = new Inch(1.0);
+        Assert.assertNotEquals(inch1,inch2);
+    }
+
+    @Test
+    public void givenReferenceForInch_ShouldReturnEqual() {
+        Inch inch1 = new Inch(0.0);
+        Inch inch2 = inch1;
+        Assert.assertEquals(inch1, inch2);
+    }
+
+    @Test
+    public void givenNullForInch_ShouldReturnFalse() {
+        Inch inch1 = new Inch(0.0);
+        Inch inch2 = null;
+        Assert.assertNotEquals(inch1,inch2);
+    }
 }
